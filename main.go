892c378d9e4a27d83dc1439e5ce3f28e62a48e64
Justin Korkie <justinjkorkie@gmail.com>
2024-11-09 22:59:15 +0200

    feat(main): add buttons

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// TODO: Move this to models
+type Button struct {
+	x      int
+	y      int
+	width  int
+	height int
+}
+
 func main() {
 	var screenWidth int32 = 800
 	var screenHeight int32 = 450
 	rl.InitWindow(screenWidth, screenHeight, "Wifoxide")
 	defer rl.CloseWindow()
+	var index int = 0
 
 	rl.SetTargetFPS(60)
 
@@ -36,13 +45,38 @@ func main() {
 			}
 		}
 
-		// Print the output
-
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-
-		rl.DrawRectangle((screenWidth+400)/2, screenHeight-85, 90, 30, rl.LightGray)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		// rl.DrawRectangle((screenWidth+400)/2, screenHeight-85, 90, 30, rl.LightGray)
+
+		var buttonHeight float32 = 60
+		var buttonSpacing float32 = 10
+		var ycoord float32 = 20
+		ssids := []string{"helloo", "sdfsdf", "hwewe"}
+		for i := 0; i < len(ssids); i++ {
+			buttonColor := rl.DarkGray
+			buttonHoverColor := rl.LightGray
+			button := rl.Rectangle{X: 50,
+				Y: ycoord + (buttonHeight+buttonSpacing)*float32(i), Width: 600,
+				Height: buttonHeight}
+
+			if rl.CheckCollisionPointRec(rl.GetMousePosition(), button) {
+				buttonColor = buttonHoverColor
+				if rl.IsMouseButtonPressed(rl.MouseLeftButton) && !(len(ssids) > 0) {
+					// cmd := ssids[0].path + " -P " + profiles[index].name + " &"
+					// openBrowserAndCloseProgram(db, &convertableTexture, cmd)
+				}
+			} else {
+				buttonColor = rl.DarkGray
+			}
+			if index == i {
+				buttonColor = rl.LightGray
+			}
+			rl.DrawRectangleRounded(button, 0.3, 10, buttonColor)
+			rl.DrawText(ssids[i], int32(button.X)+30, int32(button.Y)+15, 20, rl.Black)
+		}
 
 		rl.EndDrawing()
 	}
